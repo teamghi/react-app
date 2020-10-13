ae4e05d877ae3756288742c35b187df9abb9161d
TyHVB <tyhvb@fabatechnology.com>
2020-10-13 10:40:16 +0700

    Toogle logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo1 from './logo.svg';
+import logo2 from './logo512.png';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [logoSrc, setLogoSrc] = useState('');
+    
+    const changeSrc = () => {
+        if (logoSrc === logo1)
+        {
+            setLogoSrc(logo2);
+        } else {
+            setLogoSrc(logo1);
+        }
+    };
+    
+    return (
+        <div className="App">
+            <h1>Hello world !</h1>
+            <button onClick={changeSrc}>Toggle logo</button>
+            <Logo logoSrc={logoSrc}/>
+        </div>
+    );
+}
+
+function Logo(props) {
+    return (
+        <img src={props.logoSrc} alt={'logo'}/>
+    );
 }
 
 export default App;
